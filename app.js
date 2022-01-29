4ce9735f8662c3355ccfb62e5836dae85fdab1cf
YefersonR <Yefersonrubio27@gmail.com>
2022-01-29 01:26:28 -0400

    functionalities finished

--- app.js
+++ app.js
@@ -1,9 +1,72 @@
 var dato = document.getElementById("dato")
 var dato1 = document.getElementById("dato1")
+
 var mostrarHistorial= document.getElementById("historial")
 var igual = document.getElementById("igual")
 historial = []
+nums = []
+let values = 0
+//BototnesOperaciones
+var suma= document.getElementById('suma')
+var resta = document.getElementById('resta')
+var mult= document.getElementById('mult')
+var division = document.getElementById('division')
+//
 
+
+//Operaciones
+
+ var opSuma = ()=>{
+    if(dato1.value == ''){
+        dato1.value += `${dato.value}`
+    }
+    else{
+        dato1.value += `+ ${dato.value}`
+    }
+    values += parseInt(dato.value)
+    
+    console.log(values)
+    return values
+    };
+var opResta = ()=>{
+    
+    dato1.value += `- ${dato.value}`
+    values -= parseInt(dato.value) 
+    console.log(values)
+    return values
+
+}
+
+var opMult =()=>{
+    if(dato1.value == ''){
+        dato1.value += `${dato.value}`
+    }
+    else{
+        dato1.value += `* ${dato.value}`
+    }
+    values *= parseInt(dato.value)
+    console.log(values)
+    return values
+}
+var opDiv =()=>{
+    if(dato1.value == ''){
+        dato1.value += `${dato.value}`
+    }
+    else{
+        dato1.value += `/ ${dato.value}`
+    }
+    values /= parseInt(dato.value)
+    console.log(values)
+    return values
+}
+
+
+
+
+//
+var limpiar = ()=>{
+    dato.value = ''
+}
 
 
 //FuncionesGuardado
@@ -28,14 +91,33 @@ var guardarLS = ()=>{
 }
 
 
-
-
-
-
 //EventListeners
+suma.addEventListener('click',()=>{
+    opSuma()
+    limpiar()
+
+})
+resta.addEventListener('click',()=>{
+    opResta()
+    limpiar()
+})
+mult.addEventListener('click',()=>{
+    opMult()
+    limpiar()
+})
+division.addEventListener('click',()=>{
+    opDiv()
+    limpiar()
+})
+
+
+
 igual.addEventListener('click',()=>{
-    crearOperacion(dato.value)
+    crearOperacion(`${dato1.value} = ${values}`)
+    values = 0
     guardarLS()
+    limpiar()
+    dato1.value = ''
 })
 
 
